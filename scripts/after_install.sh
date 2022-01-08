4c285806fbe2f4fef62ae206c1640fadd5ac3275
Arc <arc.ranges@live.ca>
2022-01-07 20:40:52 -0800

    [aws] final deploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+cd /home/ec2-user/app-front-end
+npm install
+npm install pm2 -g
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/ec2-user/app-front-end
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 sudo nvm install 16.13.0
 sudo nvm install node
