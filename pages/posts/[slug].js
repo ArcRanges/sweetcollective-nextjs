f707d89bb976b083a526ce3ead08c964464b4947
Arc <arc.ranges@live.ca>
2022-01-08 12:57:54 -0800

    changed port

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,35 +1,36 @@
 import { BASE_API_URL } from "settings/constants";
 
 export default function Post({ post }) {
-  return <div>{JSON.stringify(post)}</div>;
+  // return <div>{JSON.stringify(post)}</div>;
+  return <div>post page</div>;
 }
 
-export async function getStaticPaths() {
-  const { data: posts } = await (
-    await fetch(`${BASE_API_URL}/api/posts`)
-  ).json();
+// export async function getStaticPaths() {
+//   const { data: posts } = await (
+//     await fetch(`${BASE_API_URL}/api/posts`)
+//   ).json();
 
-  const paths = posts.map((post) => ({
-    params: { slug: post.attributes.slug },
-  }));
+//   const paths = posts.map((post) => ({
+//     params: { slug: post.attributes.slug },
+//   }));
 
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-export async function getStaticProps({ params }) {
-  //   console.log(params);
-  const { slug } = params;
+// export async function getStaticProps({ params }) {
+//   //   console.log(params);
+//   const { slug } = params;
 
-  const { data } = await (
-    await fetch(`${BASE_API_URL}/api/posts?filters[slug][$eq]=${slug}`)
-  ).json();
+//   const { data } = await (
+//     await fetch(`${BASE_API_URL}/api/posts?filters[slug][$eq]=${slug}`)
+//   ).json();
 
-  const post = data[0];
+//   const post = data[0];
 
-  return {
-    props: { post },
-  };
-}
+//   return {
+//     props: { post },
+//   };
+// }
