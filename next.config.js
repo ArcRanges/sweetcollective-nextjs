270c18957e897b008157e94ee39d92079dfa85b2
Arc Ranges <arc.ranges@live.ca>
2022-02-15 20:02:03 -0800

    added antd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withAntdLess = require("next-plugin-antd-less");
 
 module.exports = withAntdLess({
-  modifyVars: { "@primary-color": "#04f" }, // optional
+  // modifyVars: { "@primary-color": "#04f" }, // optional
   lessVarsFilePath: "./src/styles/variables.less", // optional
   lessVarsFilePathAppendToEndOfContent: false, // optional
   // optional https://github.com/webpack-contrib/css-loader#object
--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -1,10 +1,76 @@
-import Layout from "containers/Layout/Layout";
+import Container from "components/Container";
+import Layout from "containers/Layout";
 import React from "react";
+
+import { Collapse } from "antd";
+
+const { Panel } = Collapse;
 
 export default function index() {
   return (
     <Layout>
-      <div>I am about page</div>
+      <Container className="py-10">
+        <div className="grid grid-cols-2 gap-8">
+          <div>
+            <p className="font-bold">ORDERS</p>
+            <Collapse defaultActiveKey={["1"]}>
+              <Panel header="This is panel header 1" key="1">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
+                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
+                  Nostrum id explicabo animi quas tempore.
+                </p>
+              </Panel>
+              <Panel header="This is panel header 2" key="2">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
+                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
+                  Nostrum id explicabo animi quas tempore.
+                </p>
+              </Panel>
+              <Panel header="This is panel header 3" key="3">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
+                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
+                  Nostrum id explicabo animi quas tempore.
+                </p>
+              </Panel>
+            </Collapse>
+          </div>
+          <div>
+            <p className="font-bold">SHIPPING</p>
+            <Collapse defaultActiveKey={["1"]}>
+              <Panel header="This is panel header 1" key="1">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
+                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
+                  Nostrum id explicabo animi quas tempore.
+                </p>
+              </Panel>
+              <Panel header="This is panel header 2" key="2">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
+                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
+                  Nostrum id explicabo animi quas tempore.
+                </p>
+              </Panel>
+              <Panel header="This is panel header 3" key="3">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
+                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
+                  Nostrum id explicabo animi quas tempore.
+                </p>
+              </Panel>
+            </Collapse>
+          </div>
+        </div>
+      </Container>
     </Layout>
   );
 }
