8bf4e9f53aa2423140c7de1bc43c32739bfa9832
Arc Ranges <arc.ranges@live.ca>
2023-01-20 20:47:01 -0800

    updated FAQ page

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -3,132 +3,45 @@ import Layout from "containers/Layout";
 import React from "react";
 
 import { Collapse } from "antd";
+import { client } from "pages/api/client";
 
 const { Panel } = Collapse;
 
-export default function index() {
+export default function index({ faqs }) {
   return (
     <Layout>
       <Container className="px-5 py-10">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div>
-            <p className="font-bold">PAYMENT</p>
-            <Collapse defaultActiveKey={["1"]}>
-              <Panel header="This is panel header 1" key="1">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-              <Panel header="This is panel header 2" key="2">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-              <Panel header="This is panel header 3" key="3">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-            </Collapse>
-          </div>
-          <div>
-            <p className="font-bold">SHIPPING</p>
-            <Collapse defaultActiveKey={["1"]}>
-              <Panel header="This is panel header 1" key="1">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-              <Panel header="This is panel header 2" key="2">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-              <Panel header="This is panel header 3" key="3">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-            </Collapse>
-          </div>
-          <div>
-            <p className="font-bold">REFUNDS</p>
-            <Collapse defaultActiveKey={["1"]}>
-              <Panel header="This is panel header 1" key="1">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-              <Panel header="This is panel header 2" key="2">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-              <Panel header="This is panel header 3" key="3">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-            </Collapse>
-          </div>
-          <div>
-            <p className="font-bold">MISC</p>
-            <Collapse defaultActiveKey={["1"]}>
-              <Panel header="This is panel header 1" key="1">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-              <Panel header="This is panel header 2" key="2">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-              <Panel header="This is panel header 3" key="3">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur dolores tempore quaerat, fuga aperiam, ducimus
-                  voluptatem nisi quos nihil, odit commodi ratione ex veritatis?
-                  Nostrum id explicabo animi quas tempore.
-                </p>
-              </Panel>
-            </Collapse>
-          </div>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+          {faqs.map((faq: any, i: number) => (
+            <div>
+              <p className="font-bold" key={i}>
+                {faq.fields.title}
+              </p>
+              <Collapse>
+                {faq.fields.entries.map((content: any, key: number) => (
+                  <Panel header={content.fields.title} key={key}>
+                    <p>{content.fields.content}</p>
+                  </Panel>
+                ))}
+              </Collapse>
+            </div>
+          ))}
         </div>
       </Container>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const { items: faqs } = (await client.getEntries({
+    content_type: "faqCollection",
+    order: "sys.createdAt",
+  })) as any;
+
+  return {
+    props: {
+      faqs,
+    },
+    revalidate: 10,
+  };
 }
